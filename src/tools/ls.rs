bcc1c21f87fa1db99e1caac3cf1bdb806289f6fe
Mirza-Samad-Ahmed-Baig <samadahmed87@gmail.com>
2025-06-28 01:55:37 +0500

    Fix: Remove mention of 'ignore' parameter from Ls tool description in ls.rs as it's not in schema

--- src/tools/ls.rs
+++ src/tools/ls.rs
@@ -13,7 +13,7 @@ impl AiTool for Ls {
     fn description(&self) -> Option<&'static str> {
         Some(
             r#"
-Lists files and directories in a given path. The path parameter must be an absolute path, not a relative path. You can optionally provide an array of glob patterns to ignore with the ignore parameter. You should generally prefer the Glob and Grep tools, if you know which directories to search.
+Lists files and directories in a given path. The path parameter must be an absolute path, not a relative path. You should generally prefer the Glob and Grep tools, if you know which directories to search.
         "#,
         )
     }
