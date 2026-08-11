0aef9fb4a3c6d8b606b40432cb9a589f7a68c187
lommix <info@lommix>
2026-08-11 11:22:54 +0200

    simplified skill tool prompt

--- src/tools/skill.zig
+++ src/tools/skill.zig
@@ -5,9 +5,7 @@ pub const LoadSkillTool = r.prv.tool.Tool{
     .def = .{
         .name = "load_skill",
         .description =
-        \\Load a specialized skill when the task at hand matches one of the skills listed in the system prompt.
-        \\Use this tool to inject the skill's instructions and resources into current conversation. The output may contain detailed workflow guidance as well as references to scripts, files, etc in the same directory as the skill.
-        \\The skill name must match one of the skills listed in your system prompt.
+        \\Load a specialized skill into the chat context.
         \\
         ,
         .parameters_schema =
